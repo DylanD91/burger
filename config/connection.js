7dd806d2eae8bf6bc6f79d5a8e4314d5fc1a2c60
Dylan Dusenberry <dylandusenberry975@gmail.com>
2019-03-27 14:39:48 -0500

    redoing

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,12 @@
 // Dependencies
-const mysql = require("mysql");
-const connection;
+var mysql = require("mysql");
+var connection;
 
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
+}else{
 // Connection to DB
-    var connection = mysql.createConnection({
+        connection = mysql.createConnection({
         host: "localhost",
         port: 3306,
         user: "root",
